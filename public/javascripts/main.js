12989d1b9488a6d53bf7c604a8a17bc2a36036c7
markkizz <mark.pattanapara@gmail.com>
2019-11-08 09:24:59 +0700

    set path href to profile

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(()=> {
+    $('#post-area').on('click', () => {
+        $('.bg-modal').show();
+    });
+
+    $('#cancel').on('click', () => {
+        $('.bg-modal').hide();
+    });
+
+
+
+});
